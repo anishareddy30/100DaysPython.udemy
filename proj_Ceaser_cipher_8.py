6e4ed99a09282fa86855b8ccf9b28378cebadaa8
anisha <aniiishaa30@gmail.com>
2026-01-11 22:08:42 +0530

    Update proj_Ceaser_cipher_8.py

--- proj_Ceaser_cipher_8.py
+++ proj_Ceaser_cipher_8.py
@@ -15,6 +15,9 @@
 # The final encrypted output should be:
 # Here is the encoded result: ifmmp
 #  Where each of the fetters of 'hello' is shifted up by 1.
+# Call the encrypt() function and pass in the user inputs. You should be able to test the code and encrypt a message.
+
+
 
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -22,4 +25,15 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+
+# Inside the 'encrypt' function, shift each letter of the original_text forwards in
+# the alphabet by the shift_amount and print the encrypted text.
 def encrypt(original_text , shift_amount):
+    cipher_text = ""
+
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) + shift_amount
+# Call the encrypt() function and pass in the user inputs. You should be able to test the code and encrypt a message.
+        shifted_position %= len(alphabet)
+        cipher_text = alphabet(shifted_position)
+    print(f"here is the encoded result : {cipher_text}")
