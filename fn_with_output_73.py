42a5641cf9e849b1fae5152aa875d64fcfacbd57
anisha <aniiishaa30@gmail.com>
2026-01-16 23:49:34 +0530

    Add fn_with_output_73.py

---
+++ fn_with_output_73.py
@@ -0,0 +1,14 @@
+def function_name (first_name , last_name):
+    print(first_name.title())
+    print(last_name.title())
+
+function_name("MANaLI" , "REddy")   #it made the first letter capital only 
+
+
+def function_name (first_name , last_name ):
+    formated_name = (first_name.title())
+    formated_name2 = (last_name.title())
+    print(f"{formated_name} {formated_name2}")
+
+function_name ("MANaki" , "REDDy")    
+
