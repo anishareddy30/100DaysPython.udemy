1958fe7a00a062d969ff3b9ac33de9aaf4f8e65c
anisha <aniiishaa30@gmail.com>
2026-06-14 12:00:00 +0530

    Work day 14: add code files

---
+++ 22_if _else.py
@@ -0,0 +1,5 @@
+height = int(input("whats your height"))
+if height >= 180 :
+    print("you r eligible")
+else:
+    print("sorry , grow taller")    
---
+++ 24_nested_if.py
@@ -0,0 +1,10 @@
+height = int(input("whats your height ?"))
+if height >= 120 :
+    print("can ride")
+    age = int(input("whats your age "))
+    if age >= 18 :
+        print("pay $7")
+    else:
+        print("pay $10")    
+else:
+    print("cant ride")    
