35160abc7b2f77ede7afde85d295d50cc1819c93
anisha <aniiishaa30@gmail.com>
2026-06-17 12:00:00 +0530

    Work day 17: add code files

---
+++ 61.fn.py
@@ -0,0 +1,5 @@
+def greet (name ):
+    print(f"hello{name}")
+    print(f"how r u {name} ? ")
+
+greet("anisha")    
---
+++ 7_string_concatenation.py
@@ -0,0 +1,5 @@
+name = "anisha"
+age="20"
+print(f"my name is {name } and my age is {age}")
+print(name + age)
+print(name + " " +age)
